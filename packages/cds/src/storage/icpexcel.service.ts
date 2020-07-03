168b02a35cd0d4079d1b1f69bb56d44ed38c7cea
penglin <519645581@qq.com>
2020-07-03 17:11:08 +0800

    add storage service

--- packages/cds/src/storage/icpexcel.service.ts
+++ packages/cds/src/storage/icpexcel.service.ts
@@ -4,27 +4,23 @@ import { Observable } from 'rxjs';
 import { BaseApi, BaseUrl, DELETE, GET, Payload, POST, PUT } from '@co/common';
 import { ListResultDto, PagedResultDto } from '@co/core';
 
-import {  } from './storage.types';
+import {} from './storage.types';
 
 @BaseUrl('/storage/ICPExcel')
 @Injectable({ providedIn: 'root' })
 export class ICPExcelService extends BaseApi {
+  /**
+   * @param url /Storage/ICPExcel/OrderImport
+   * 提供给ICP导入在线解析，返回数据列表
+   */
 
-   
-    /**
-     * @param url /Storage/ICPExcel/OrderImport
-     * 提供给ICP导入在线解析，返回数据列表
-     */
-
-    @POST('orderImport')
-    orderImport(
-        @Payload
-        _req: {file?:file} 
-
-    ): Observable<any> {
-        return null as any
-    }
-
-
-
+  @POST('orderImport')
+  orderImport(
+    @Payload
+    _req: {
+      file?: any;
+    },
+  ): Observable<any> {
+    return null as any;
   }
+}
