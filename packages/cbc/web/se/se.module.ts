61cabe655095ef6312032ef1fcd71822153a740a
huangdi <edisonhuang@cityocean.com>
2020-07-08 10:47:59 +0800

    feat(cbc): co-validation 表单验证popover 提示

--- packages/cbc/web/se/se.module.ts
+++ packages/cbc/web/se/se.module.ts
@@ -7,12 +7,15 @@ import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { SEContainerComponent } from './se-container.component';
 import { SETitleComponent } from './se-title.component';
 import { SEComponent } from './se.component';
+import { CoValidationDirective } from './co-validation.directive';
 
 const COMPONENTS = [SEContainerComponent, SEComponent, SETitleComponent];
+const DIRECTIVES = [CoValidationDirective];
+const EXPORTS = [...COMPONENTS, ...DIRECTIVES];
 
 @NgModule({
   imports: [CommonModule, CoUtilModule, NzToolTipModule, NzIconModule, NzOutletModule],
-  declarations: [...COMPONENTS],
-  exports: [...COMPONENTS],
+  declarations: [...EXPORTS],
+  exports: [...EXPORTS],
 })
 export class SEModule {}
