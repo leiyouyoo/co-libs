6db7113ea3f741c0cb5af656c51a4108382c9347
huangdi <edisonhuang@cityocean.com>
2020-07-08 15:16:07 +0800

    fix(cbc): multiple entry

--- packages/cbc/business/business.module.ts
+++ packages/cbc/business/business.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { ContainerPickerModule } from '@co/cbc/business/container-picker';
 import { DataDictionaryPickerModule } from '@co/cbc/business/data-dictionary-picker';
 import { CdsModule } from '@co/cds';
-import { ChargingCodePickerModule } from './charging-code-picker';
-import { CountyPickerModule } from './county-picker';
-import { CurrencyPickerModule } from './currency-picker';
-import { CustomerPickerModule } from './customer-picker';
-import { FlightPickerModule } from './flight-picker';
-import { PlacePickerModule } from './place-picker';
-import { AreaPickerModule } from './area-picker';
-import { RegionPickerModule } from './region-picker';
-import { PortPickerModule } from './port-picker';
-import { ProductPickerModule } from './product-picker';
-import { ShipnamePickerModule } from './shipname-picker';
-import { YoyagePickerModule } from './voyage-picker';
+import { ChargingCodePickerModule } from '@co/cbc/business/charging-code-picker';
+import { CountyPickerModule } from '@co/cbc/business/county-picker';
+import { CurrencyPickerModule } from '@co/cbc/business/currency-picker';
+import { CustomerPickerModule } from '@co/cbc/business/customer-picker';
+import { FlightPickerModule } from '@co/cbc/business/flight-picker';
+import { PlacePickerModule } from '@co/cbc/business/place-picker';
+import { AreaPickerModule } from '@co/cbc/business/area-picker';
+import { RegionPickerModule } from '@co/cbc/business/region-picker';
+import { PortPickerModule } from '@co/cbc/business/port-picker';
+import { ProductPickerModule } from '@co/cbc/business/product-picker';
+import { ShipnamePickerModule } from '@co/cbc/business/shipname-picker';
+import { YoyagePickerModule } from '@co/cbc/business/voyage-picker';
 
 const EXPORT_MODULES: any[] = [
   CustomerPickerModule,
