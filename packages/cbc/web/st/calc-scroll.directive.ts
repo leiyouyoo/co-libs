5158047885927cb305d34b6fc1438013897bc2c2
huangdi <edisonhuang@cityocean.com>
2020-07-09 20:14:28 +0800

    feat(cbc): st auto calc [scroll]

---
+++ packages/cbc/web/st/calc-scroll.directive.ts
@@ -0,0 +1,58 @@
+import {
+  AfterContentInit,
+  AfterViewChecked,
+  ChangeDetectorRef,
+  Directive,
+  ElementRef,
+  HostListener,
+  Input, Optional,
+} from '@angular/core';
+import { debounce, InputBoolean } from '@co/core';
+import { NzTableComponent } from 'ng-zorro-antd';
+import { STComponent } from './st.component';
+
+@Directive({
+  selector: 'co-st[calcScroll],nz-table[calcScroll]'
+})
+export class CalcScrollDirective implements AfterContentInit, AfterViewChecked {
+  @Input() @InputBoolean() disableCalcX = false;
+  el: HTMLElement;
+  lastHeight = 0;
+
+  constructor(private host: ElementRef,
+              @Optional() private _nzTableComponent: NzTableComponent,
+              @Optional() private stComponent: STComponent,
+              ) {}
+
+  get nzTableComponent() {
+    return this._nzTableComponent ?? this.stComponent.orgTable;
+  }
+
+  ngAfterContentInit(): void {
+    this.el = this.host.nativeElement;
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.lastHeight !== this.el.clientHeight) {
+      this.lastHeight = this.el.clientHeight;
+      this.calc();
+    }
+  }
+
+  // todo performance
+  @HostListener('window:resize')
+  @debounce()
+  calc() {
+    setTimeout(() => {
+      const thead = this.el.querySelector('.ant-table-thead');
+      this.nzTableComponent.nzScroll = {
+        x: this.disableCalcX ? this.nzTableComponent.scrollX : `${this.el.clientWidth - 17}px`,
+        y: `${this.el.clientHeight - thead!.clientHeight}px`,
+      };
+      this.nzTableComponent.ngOnChanges({ nzScroll: this.nzTableComponent.nzScroll } as any);
+      // hack
+      //@ts-ignore
+      (this.nzTableComponent.cdr as ChangeDetectorRef).markForCheck();
+    });
+  }
+}
