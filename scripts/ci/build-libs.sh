39b2adb7c206927db76901820fa16dbae6ce7888
huangdi <edisonhuang@cityocean.com>
2020-07-13 11:46:05 +0800

    release: v0.0.77

--- scripts/ci/build-libs.sh
+++ scripts/ci/build-libs.sh
@@ -37,16 +37,16 @@ done
 
 buildLess() {
   echo 'copy styles...'
-  node ./scripts/build/copy-less.js
+  node --max_old_space_size=5120 ./scripts/build/copy-less.js
 
   echo 'fix zorro path...'
-  node ./scripts/build/fix-zorro-path.js
+  node --max_old_space_size=5120 ./scripts/build/fix-zorro-path.js
 
   echo 'build full css...'
-  node ./scripts/build/generate-css.js
-  
+  node --max_old_space_size=5120 ./scripts/build/generate-css.js
+
   echo 'generate less var...'
-  node ./scripts/build/generate-less-vars.js
+  node --max_old_space_size=5120 ./scripts/build/generate-less-vars.js
 }
 
 containsElement () {
