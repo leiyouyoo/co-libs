18455630f0f978ec670b698e7160697f2820c128
huangdi <edisonhuang@cityocean.com>
2020-08-12 10:46:52 +0800

    refactor(common): user config service

--- packages/common/src/services/settings/user-custom-config.service.ts
+++ packages/common/src/services/settings/user-custom-config.service.ts
@@ -1,34 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { _HttpClient } from '../http/http.client';
-import { deepGet, deepMergeKey } from '@co/core';
+import { CO_SESSIONSERVICE_TOKEN, deepGet, deepMergeKey, ISessionService } from '@co/core';
 import { map, tap } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { set as _Set, cloneDeep } from 'lodash';
 
 @Injectable({ providedIn: 'root' })
 export class UserCustomConfigService {
-  configKey = `PageSets.FCM.Page1`;
-  value: any = void 0;
+  get value(): string {
+    return this.sessionService.settings[this.currentRouteUrl];
+  }
+  get currentRouteUrl() {
+    return this.router.url.split('#')[0];
+  }
 
   constructor(private http: _HttpClient,
               private activatedRoute: ActivatedRoute,
               private router: Router,
+              @Inject(CO_SESSIONSERVICE_TOKEN) private sessionService: ISessionService,
   ) {
   }
 
   getByPath(path: string | string[], defaultValue?: any): any {
-    return deepGet(this.value, this.pathFactory(path), defaultValue);
+    const config: string = this.value;
+    if (config) {
+      return deepGet(JSON.parse(config), this.pathFactory(path), defaultValue);
+    } else {
+      return defaultValue;
+    }
   }
 
-  getCurrentUserSetting(): Promise<any> {
-    return this.http.get('/platform/Setting/getCurrentUserSetting', { key: this.configKey })
+  private getCurrentUserSetting(key?: string): Promise<any> {
+    return this.http.get('/platform/Setting/getCurrentUserSetting', { key: key || this.currentRouteUrl })
       .pipe(
         map(data => {
           try {
-            this.value = JSON.parse(data);
+            // this.value = JSON.parse(data);
           } catch (e) {
-            this.value = null;
             console.error(e);
           }
           return this.value;
@@ -42,16 +51,16 @@ export class UserCustomConfigService {
     const preValue = cloneDeep(this.value);
     const mergedValue = deepMergeKey(preValue || {}, true, _Set({}, path, value));
 
-    return this.http.post('/platform/Setting/setCurrentUserSetting', { name: this.configKey, value: JSON.stringify(mergedValue) } )
+    return this.http.post('/platform/Setting/setCurrentUserSetting', { name: this.currentRouteUrl, value: JSON.stringify(mergedValue) } )
       .pipe(
         tap(() => {
-          this.value = mergedValue;
+          this.sessionService.settings[this.currentRouteUrl] = mergedValue;
         })
       ).toPromise()
   }
 
   pathFactory(path: string | string[]): string[] {
     path = Array.isArray(path) ? path : (path as string).split('.') ;
-    return [this.router.url.split('#')[0], ...(path as string[])];
+    return [...(path as string[])];
   }
 }
