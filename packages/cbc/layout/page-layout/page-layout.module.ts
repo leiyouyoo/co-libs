0da6414679bcb74b861544ef06dd9a7221970212
duanzui <811192167@qq.com>
2020-09-09 10:24:12 +0800

    解决构建问题；

--- packages/cbc/layout/page-layout/page-layout.module.ts
+++ packages/cbc/layout/page-layout/page-layout.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { CaretLeftOutline, MoreOutline } from '@ant-design/icons-angular/icons';
 import { NzOutletModule } from 'ng-zorro-antd';
 import { PortalModule } from '@angular/cdk/portal';
 import { PageLayoutComponent } from './page-layout.component';
@@ -11,7 +9,8 @@ import { PageSideResizeHandleComponent } from './page-side-resize-handle.compone
 import { PageSideDrawerComponent } from './drawer/page-side-drawer.component';
 
 
-const COMPONENTS = [PageLayoutComponent,
+const COMPONENTS = [
+  PageLayoutComponent,
   PageMainComponent,
   PageSideComponent,
   PageSideResizeHandleComponent,
@@ -22,7 +21,6 @@ const COMPONENTS = [PageLayoutComponent,
   declarations: [...COMPONENTS],
   imports: [
     CommonModule,
-    NzIconModule.forRoot([MoreOutline, CaretLeftOutline]),
     NzOutletModule,
     PortalModule,
   ],
