45757c068baf458fb61a1a666ba55df6e7462a69
penglin <519645581@qq.com>
2020-11-10 15:16:10 +0800

    fix(pub): error

--- packages/cds/src/pub/feedback.service.ts
+++ packages/cds/src/pub/feedback.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
-import { Observable } from 'rxjs';
 import { BaseApi, BaseUrl, DELETE, FORM, GET, Payload, POST, PUT } from '@co/common';
-import { PUBFeedbackInput,PUBFeedbackDto,PUBListResultDto, } from './pub.types';
+import { Observable } from 'rxjs';
+import { PUBListResultDto } from './pub.types';
 
 @BaseUrl('/pub/Feedback')
 @Injectable({ providedIn: 'root' })
@@ -10,96 +10,113 @@ export class PUBFeedbackService extends BaseApi {
     super(injector);
   }
 
-  
-    /**
-     * @param url /PUB/Feedback/CreateOrUpdate
-     * 创建新的反馈
-     */
+  /**
+   * @param url /PUB/Feedback/CreateOrUpdate
+   * 创建新的反馈
+   */
 
-    @POST('createOrUpdate')
-    createOrUpdate(
-        @Payload
-        _req:PUBFeedbackInput
-
-    ): Observable<PUBFeedbackDto> {
-        return null as any
-    }
-
-
-    /**
-     * @param url /PUB/Feedback/Get
-     * 获取详情
-     */
-
-    @GET('get')
-    get(
-        @Payload
-        _req: {id?:string} 
-
-    ): Observable<PUBFeedbackDto> {
-        return null as any
-    }
-
-
-    /**
-     * @param url /PUB/Feedback/GetFeedbackStatistics
-     * 反馈统计
-     */
-
-    @GET('getFeedbackStatistics')
-    getFeedbackStatistics(
-        @Payload
-        _req: {title?:string,feedbackTypeId?:number,feedbackSource?:string,userId?:number,startDate?:string,endDate?:string,isHandle?:boolean,ids?:any[],keyName?:string,searchText?:string,includeDeleted?:boolean,sorting?:string,maxResultCount?:number,skipCount?:number} 
-
-    ): Observable<any> {
-        return null as any
-    }
-
-
-    /**
-     * @param url /PUB/Feedback/GetFeedTypeList
-     * 获取反馈类型列表
-     */
-
-    @GET('getFeedTypeList')
-    getFeedTypeList(
-        @Payload
-        _req: {} 
-
-    ): Observable<any> {
-        return null as any
-    }
-
-
-    /**
-     * @param url /PUB/Feedback/GetList
-     * 查询列表数据 分页
-     */
-
-    @GET('getList')
-    getList(
-        @Payload
-        _req: {title?:string,feedbackTypeId?:number,feedbackSource?:string,userId?:number,startDate?:string,endDate?:string,isHandle?:boolean,ids?:any[],keyName?:string,searchText?:string,includeDeleted?:boolean,sorting?:string,maxResultCount?:number,skipCount?:number} 
-
-    ): Observable<PUBListResultDto<PUBFeedbackDto>> {
-        return null as any
-    }
-
-
-    /**
-     * @param url /PUB/Feedback/HandleFeedback
-     * 处理反馈
-     */
-
-    @POST('handleFeedback')
-    handleFeedback(
-        @Payload
-        _req:PUBFeedbackInput
-
-    ): Observable<any> {
-        return null as any
-    }
-
-
-
+  @POST('createOrUpdate')
+  createOrUpdate(
+    @Payload
+    _req: any,
+  ): Observable<any> {
+    return null as any;
   }
+
+  /**
+   * @param url /PUB/Feedback/Get
+   * 获取详情
+   */
+
+  @GET('get')
+  get(
+    @Payload
+    _req: {
+      id?: string;
+    },
+  ): Observable<any> {
+    return null as any;
+  }
+
+  /**
+   * @param url /PUB/Feedback/GetFeedbackStatistics
+   * 反馈统计
+   */
+
+  @GET('getFeedbackStatistics')
+  getFeedbackStatistics(
+    @Payload
+    _req: {
+      title?: string;
+      feedbackTypeId?: number;
+      feedbackSource?: string;
+      userId?: number;
+      startDate?: string;
+      endDate?: string;
+      isHandle?: boolean;
+      ids?: any[];
+      keyName?: string;
+      searchText?: string;
+      includeDeleted?: boolean;
+      sorting?: string;
+      maxResultCount?: number;
+      skipCount?: number;
+    },
+  ): Observable<any> {
+    return null as any;
+  }
+
+  /**
+   * @param url /PUB/Feedback/GetFeedTypeList
+   * 获取反馈类型列表
+   */
+
+  @GET('getFeedTypeList')
+  getFeedTypeList(
+    @Payload
+    _req: {},
+  ): Observable<any> {
+    return null as any;
+  }
+
+  /**
+   * @param url /PUB/Feedback/GetList
+   * 查询列表数据 分页
+   */
+
+  @GET('getList')
+  getList(
+    @Payload
+    _req: {
+      title?: string;
+      feedbackTypeId?: number;
+      feedbackSource?: string;
+      userId?: number;
+      startDate?: string;
+      endDate?: string;
+      isHandle?: boolean;
+      ids?: any[];
+      keyName?: string;
+      searchText?: string;
+      includeDeleted?: boolean;
+      sorting?: string;
+      maxResultCount?: number;
+      skipCount?: number;
+    },
+  ): Observable<PUBListResultDto<any>> {
+    return null as any;
+  }
+
+  /**
+   * @param url /PUB/Feedback/HandleFeedback
+   * 处理反馈
+   */
+
+  @POST('handleFeedback')
+  handleFeedback(
+    @Payload
+    _req: any,
+  ): Observable<any> {
+    return null as any;
+  }
+}
