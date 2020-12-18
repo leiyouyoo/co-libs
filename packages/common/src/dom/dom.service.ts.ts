9a36e0b2639ee935b80defd227335a47c89f4da2
duanzui <811192167@qq.com>
2020-12-18 13:54:34 +0800

    通过dom获取组件实例的方法，封装service

---
+++ packages/common/src/dom/dom.service.ts.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class CoDomService {
+
+  MONKEY_PATCH_KEY_NAME = '__ngContext__';
+
+  /**
+   * 根据dom对象获取组件实例，这个dom是组件下面的子元素，不是组件自身节点
+   * 比如：<app-compA><div #div></div></app-compA>，可以通过div获取CompA组件实例
+   * @param element dom对象
+   * @returns {any} 组件实例，注意判断是否为空
+   */
+  findAngularComponent(element: HTMLElement) {
+    let comp;
+    let process = true;
+    while (process) {
+      if (element[this.MONKEY_PATCH_KEY_NAME]) {
+        if (element[this.MONKEY_PATCH_KEY_NAME][0]) {
+          comp = element[this.MONKEY_PATCH_KEY_NAME][8];
+        } else {
+          // app-root component
+          comp = null;
+        }
+        process = false;
+      } else {
+        element = element.parentNode as HTMLElement;
+      }
+    }
+    return comp;
+  }
+
+}
---
+++ packages/common/src/dom/index.zh-CN.md
@@ -0,0 +1,9 @@
+---
+order: 3
+title: dom服务
+subtitle: dom相关操作
+type: Service
+---
+
+根据dom对象获取组件实例，这个dom可以是组件下面的子元素，也可以是组件自身节点
+> 注: 注意判断返回结果是否为空
