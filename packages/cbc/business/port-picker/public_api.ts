5ad7c45bdbdc5be9bf951588da56f0c2667f3570
huangdi <edisonhuang@cityocean.com>
2021-03-05 16:54:47 +0800

    feat(cbc): port picker

--- packages/cbc/business/port-picker/public_api.ts
+++ packages/cbc/business/port-picker/public_api.ts
@@ -1,2 +1,3 @@
 export { PortPickerComponent } from './port-picker.component';
+export { PortTemplateComponent as ɵPortTemplateComponent } from './port-template.component';
 export { PortPickerModule } from './port-picker.module';
