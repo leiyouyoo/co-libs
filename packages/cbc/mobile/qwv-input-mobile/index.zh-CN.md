8c3768a8e085078bd5539a3282a8b6794baa8034
huangdi <edisonhuang@cityocean.com>
2021-03-15 17:04:42 +0800

    feat(mobile): qwv-input-mobile

---
+++ packages/cbc/mobile/qwv-input-mobile/index.zh-CN.md
@@ -0,0 +1,18 @@
+---
+type: Mobile
+title: qwv-input-mobile
+order: 8
+subtitle: 件毛体输入
+cols: 2
+module: import { QwvInputModule } from '@co/cbc';
+---
+
+承运人选择器。
+
+## API
+
+### qwv-input-mobile
+
+| 成员 | 说明 | 类型 | 默认值 |
+|----|----|----|-----|
+| `[(ngModel)]` | 值绑定 | any | null |
---
+++ packages/cbc/mobile/qwv-input-mobile/qwv-input-mobile.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QwvInputMobileComponent } from './qwv-input-mobile.component';
+
+
+@NgModule({
+  declarations: [
+    QwvInputMobileComponent,
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    QwvInputMobileComponent,
+  ],
+})
+export class QwvInputMobileModule { }
