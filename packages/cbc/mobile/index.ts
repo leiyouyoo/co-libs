91cf8e7af29c62322c637a0af55c1c5ceee02fe4
penglin <519645581@qq.com>
2021-03-16 17:18:25 +0800

    release v1.1.32

--- packages/cbc/mobile/index.ts
+++ packages/cbc/mobile/index.ts
@@ -1,3 +1,8 @@
+export * from '@co/cbc/mobile/commodity-picker-mobile';
 export * from '@co/cbc/mobile/customer-picker-mobile';
+export * from '@co/cbc/mobile/customer-service-picker-mobile';
+export * from '@co/cbc/mobile/operation-port-picker-mobile';
+export * from '@co/cbc/mobile/port-picker-mobile';
+export * from '@co/cbc/mobile/qwv-input-mobile';
 
 export * from './business-mobile.module';
